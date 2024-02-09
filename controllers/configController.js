a26b48de7b5fec55f2269e23c01e85c5f7cd0465
Manish Sharma <manish.sharma@ndlm.co.in>
2024-02-09 13:10:26 +0530

    get config

--- controllers/configController.js
+++ controllers/configController.js
@@ -5,14 +5,14 @@ const apiResponse = require("../helpers/apiResponse");
 
 const getMultipleConfig = catchAsyncErrors(async (req, res, next) => {
     const { configList } = req.body;
-    const config = await Config.find({ type: { $in: configList } });
+    const config = await Config.find({ type: { $in: configList } }).populate('values');
     if (!config) {
         return apiResponse.successResponseWithData(res, 'No data found for requested configs.', []);
     }
     return apiResponse.successResponseWithData(res, null, config);
 })
 
-const postNewConfig = catchAsyncErrors(async (req, res, next) => {    
+const postNewConfig = catchAsyncErrors(async (req, res, next) => {
     const configValues = await ConfigValues.insertMany(req.body.values);
     const config = new Config(req.body);
     config.values = configValues;
--- index.js
+++ index.js
@@ -2,10 +2,19 @@ const express = require("express");
 require("dotenv").config();
 const mongoose = require("mongoose");
 const morgan = require("morgan");
+const cors = require('cors');
 const app = express();
 const cookieParser = require('cookie-parser');
 var apiRouter = require("./routes/ApiRoutes");
 //
+const corsOptions = {
+  origin: process.env.FRONT_END_URL, // Allow requests from this origin
+  methods: ['GET', 'POST', 'PATCH', 'DELETE', 'OPTIONS'],      // Allow only specified HTTP methods
+  allowedHeaders: ['Content-Type'], // Allow only specified headers
+  optionsSuccessStatus: 200      // Some legacy browsers (IE11, various SmartTVs) choke on 204
+};
+
+app.use(cors(corsOptions));
 
 app.use(cookieParser());
 
@@ -37,7 +46,7 @@ app.use("/api/", apiRouter);
 process.on('unhandledRejection', err => {
   console.log(`Error: ${err.message}`);
   console.log('Shutting down the server due to Unhandled promise rejection.')
-  server.close( () => {
-      process.exit(1);
-  }) 
+  server.close(() => {
+    process.exit(1);
+  })
 });
